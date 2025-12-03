9a52571b71ef10ffac9b955dc21b3b1db9034250
Ani <aniket.satve@adypu.edu.in>
2025-12-03 12:53:51 +0530

    Delete backend directory

--- backend/dist/api-disputes.d.ts
+++
@@ -1,3 +0,0 @@
-declare const router: import("express-serve-static-core").Router;
-export { router as disputeApi };
-//# sourceMappingURL=api-disputes.d.ts.map
--- backend/dist/api-v2.d.ts
+++
@@ -1,5 +0,0 @@
-import { Express } from 'express';
-declare const app: Express;
-export declare function startAPI(): void;
-export default app;
-//# sourceMappingURL=api-v2.d.ts.map
